9bd5d9a0998a790c8481c791d66127a349029609
Paul <trade@iamaflip.co.uk>
2017-09-24 08:25:50 +0100

    Added market/stop/limit orders and withdrawal types

--- Gdax/GdaxResponse.cs
+++ Gdax/GdaxResponse.cs
@@ -14,8 +14,9 @@ namespace Gdax
 			this.StatusCode = statusCode;
 			this.Content = content;
 
+			// Show JSON response status in output
 			Debug.WriteLine(this.StatusCode);
-			Debug.WriteLine(this.Content);
+			//Debug.WriteLine(this.Content);
 		}
 
 		public KeyValuePair<String, IEnumerable<String>>[] Headers { get; }
