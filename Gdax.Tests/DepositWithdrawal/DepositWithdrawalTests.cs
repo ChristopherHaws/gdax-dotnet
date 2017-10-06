f5968be55bff3a14dcffcf66d08be7485cdb75be
Paul <trade@iamaflip.co.uk>
2017-10-06 07:52:09 +0100

    Market Period Enum added, Deposit / Withdrawal test added

--- Gdax.Tests/DepositWithdrawal/DepositWithdrawalTests.cs
+++ Gdax.Tests/DepositWithdrawal/DepositWithdrawalTests.cs
@@ -13,89 +13,28 @@ namespace Gdax.Tests.DepositWithdrawal
 		{
 			UseSandbox = true
 		};
-		private static Guid paymentIdForWithdrawal;
+
 		private static Guid coinbase_id;
 
 		[Fact]
-		public async Task TestAllDepositWithdrawalFunctions()
+		public async Task DepositWithdrawals_ShouldReturnTheSuccess()
 		{
-			await GetPaymentsInfo().ConfigureAwait(false);
-
-			await TestDepositBank(100m, paymentIdForWithdrawal, "EUR");
-
-			await TestDepositCoinbase(100m, coinbase_id, "USD");
-
-			await TestWithdrawCoinbase(00m, coinbase_id, "USD");
-		}
-
-		public static async Task GetPaymentsInfo()
-		{
-			foreach (var b in await client.GetPaymentMethods())
+			foreach (var c in await client.GetCoinbaseAccounts())
 			{
-				// if (b.Name.Contains("EUR"))
-				// && b.Type == "fiat_account"
-
-				if (b.Type.Contains("sepa"))
+				if (c.Currency == "USD")
 				{
-					Debug.WriteLine("*******************************");
-					Debug.WriteLine("ID {0}", b.Id);
-					Debug.WriteLine("Type {0}", b.Type);
-					Debug.WriteLine("Currency {0}", b.Currency);
-					Debug.WriteLine("Primary Sell {0}", b.IsPrimarySellMethod);
-					Debug.WriteLine("Bank Name {0}", b.Name);
-					Debug.WriteLine("Allow Withdraw {0}", b.CanWithdraw);
-					Debug.WriteLine("Allow Deposit {0}", b.CanDeposit);
-					Debug.WriteLine("*******************************");
-
-					paymentIdForWithdrawal = new Guid(b.Id.ToString());
+					coinbase_id = c.Id;
 				}
 			}
 
-			foreach (var c in await client.GetCoinbaseAccounts())
-			{
-				// if (c.Currency == "GBP") // && Convert.ToDecimal(c.Balance) > 0
-				// {
-				Debug.WriteLine("*******************************");
-				Debug.WriteLine("Coinbase ID {0}", c.Id);
-				Debug.WriteLine("Coinbase Balance {0}", c.Balance);
-				Debug.WriteLine("Coinbase Currency {0}", c.Currency);
-				Debug.WriteLine("*******************************");
+			var depositFiat = await client.DepositFromCoinbase(1000m, "USD", coinbase_id).ConfigureAwait(false);
 
-				coinbase_id = new Guid(c.Id.ToString());
-			}
+			depositFiat.ShouldBeNull();
+
+			var withdrawtoCoinbase = await client.WithdrawToCoinbase(1000m, "USD", coinbase_id).ConfigureAwait(false);
+
+			withdrawtoCoinbase.ShouldBeNull();
 		}
 
-		public static async Task TestDepositBank(Decimal amount, Guid paymentID, String currency)
-		{
-			var depositFiat = await client.DepositFromBank(amount, currency, paymentID).ConfigureAwait(false);
-		}
-
-		public static async Task TestDepositCoinbase(Decimal amount, Guid coinbaseID, String currency)
-		{
-			var depositFiat = await client.DepositFromCoinbase(amount, currency, coinbaseID).ConfigureAwait(false);
-		}
-
-		public static async Task TestWithdrawBank(Decimal amoumt, Guid paymentID, String currency)
-		{
-			Debug.WriteLine("Send Sepa Withdrawal");
-
-			var withdrawSepa = await client.WithdrawToBank(amoumt, currency, paymentID);
-		}
-
-		public static async Task TestWithdrawCoinbase(Decimal amount, Guid coinbaseID, String currency)
-		{
-			Debug.WriteLine("Send Coinbase Withdrawal");
-
-			var withdrawtoCoinbase = await client.WithdrawToCoinbase(amount, currency, coinbaseID).ConfigureAwait(false);
-		}
-
-		public static async Task TestWithdrawCrypto(Decimal amount, String currency, String walletAddress)
-		{
-			Debug.WriteLine("Send Crypto Withdrawal");
-
-			walletAddress = "someCryptoAddress";
-
-			var withdrawCypto = await client.WithdrawToWallet(amount, currency, walletAddress).ConfigureAwait(false);
-		}
 	}
 }
