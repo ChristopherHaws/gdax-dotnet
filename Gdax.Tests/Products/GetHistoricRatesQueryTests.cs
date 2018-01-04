be01bf105ea9f1a37f7ce25a7ea1a05b5cf30e00
Sergio Moreno <sergio.moreno.calzada@everis.com>
2018-01-04 03:37:12 +0100

    The granularity field must be one of the following values: {60, 300, 900, 3600, 21600, 86400}.

--- Gdax.Tests/Products/GetHistoricRatesQueryTests.cs
+++ Gdax.Tests/Products/GetHistoricRatesQueryTests.cs
@@ -15,7 +15,7 @@ namespace Gdax.Products
 				UseSandbox = true
 			};
 
-			var history = await client.GetHistoricRates("BTC-USD", DateTime.UtcNow.AddMinutes(-10), DateTime.UtcNow, 30);
+			var history = await client.GetHistoricRates("BTC-USD", DateTime.UtcNow.AddMinutes(-10), DateTime.UtcNow, 60);
 
 			history.ShouldNotBeNull();
 		}
